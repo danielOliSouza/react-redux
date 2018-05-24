7f8f81e985c3b6dfeb5d8d30307cdd29b73af3a8
Daniel Oliveira <daniel.backspace@gmail.com>
2018-05-24 16:10:05 -0300

    Aula 13 Seção 2 Curso de React + Redux

--- ex/index.js
+++ ex/index.js
@@ -1,1 +1,4 @@
-const logger = require('./duvidaCruel.js');
+import Pessoa from './pessoa'; // Os Browser não suporta mais é "traduzido" para o javascript "antigo", esse é uma exemplo de ES2015
+
+const pessoa = new Pessoa('Daniel');
+console.log(pessoa.toString());
